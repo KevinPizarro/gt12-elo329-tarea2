f787c9cc7b8b55b9e33064afeb05bc771099d5e2
Kevin Pizarro <kevin.pizarroa@sansano.usm.cl>
2021-06-04 23:42:57 -0400

    Stage2 Setup

---
+++ Stage2/ComunaView.java
@@ -0,0 +1,18 @@
+package Stage2;
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class ComunaView extends Group {
+    private final Comuna comuna;
+    public ComunaView(Comuna c){
+        comuna = c;
+        Rectangle territoryView = new Rectangle(comuna.getWidth(), comuna.getHeight(), Color.WHITE);
+        territoryView.setStroke(Color.BROWN);
+        getChildren().add(territoryView);
+        setFocusTraversable(true);  // needed to receive mouse and keyboard events.
+    }
+    public void update(){
+        comuna.getPedestrian().updateView();
+    }
+}
---
+++ Stage2/PedestrianView.java
@@ -0,0 +1,24 @@
+package Stage2;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+
+public class PedestrianView {
+    private Pedestrian person;
+    private Rectangle view;
+    private final double SIZE = 5;
+
+    public PedestrianView(Comuna comuna, Pedestrian p) {
+        person = p;
+        view = new Rectangle(SIZE, SIZE, Color.BLUE);
+        view.setX(person.getX() - SIZE / 2);   // Rectangle x position is the X coordinate of the
+        // upper-left corner of the rectangle
+        view.setY(person.getY() - SIZE / 2); // Rectangle y position is the Y coordinate of the
+        // upper-left corner of the rectangle
+        comuna.getView().getChildren().addAll(view);
+    }
+
+    public void update() {
+        view.setX(person.getX() - SIZE / 2);
+        view.setY(person.getY() - SIZE / 2);
+    }
+}
