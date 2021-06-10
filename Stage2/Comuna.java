62637b04f55328c8b28cdfebbac92e8698d71f32
Manuel Cruces <manuel.cruces@sansano.usm.cl>
2021-06-10 03:11:06 +0000

    Comuna comentada

--- Stage2/Comuna.java
+++ Stage2/Comuna.java
@@ -5,21 +5,40 @@ import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
 
+/**
+ * Genera la comuna donde se realizara el análisis
+ */
 public class Comuna {
+    /**
+    * Arreglo de individuos
+    */
     private ArrayList<Pedestrian> personas;
-    private Rectangle2D territory; // Alternatively: double width, length;
-                                   // but more methods would be needed.
+    /**
+    * Dimensiones del territorio de la comuna
+    */
+    private Rectangle2D territory; 
+    /**
+     * Representación visual de la comuna
+     */
     private ComunaView view;
-    private Pane graph;
 
     public Comuna(){
+        /**
+        * Constructor de la clase Comuna
+        */
         double width = SimulatorConfig.WIDTH;
         double length = SimulatorConfig.LENGTH;
         territory = new Rectangle2D(0,0, width, length);
         double speed = SimulatorConfig.SPEED;
         double deltaAngle = SimulatorConfig.DELTA_THETA;
-        view = new ComunaView(this); // What if you exchange this and the follow line?
+        view = new ComunaView(this); 
+                /**
+        * Crea el arreglo de individuos
+        */
         personas = new ArrayList<Pedestrian>(SimulatorConfig.N);
+        /**
+        * Infecta individuos según los datos del archivos
+        */
         for(int i=0;i<SimulatorConfig.N;i++){
             if(i<SimulatorConfig.I){
                 Pedestrian p=new Pedestrian(this, speed, deltaAngle);
@@ -31,19 +50,35 @@ public class Comuna {
                 personas.add(p);
             }
         }
-        graph = new Pane();  // to be completed in other stages.
+
+
     }
+    /**
+    * retorna el ancho de la comuna
+    */
     public double getWidth() {
         return territory.getWidth();
     }
+    /**
+    * retorna el alto de la comuna
+    */
     public double getHeight() {
         return territory.getHeight();
     }
+    /**
+    * revisa el estado de los individuos y los actualiza
+    */
     public void computeNextState (double delta_t) {
         for(int i=0;i < personas.size();i++){
             for(int j=0; j< personas.size();j++){
+                /**
+                * revisa si hay individuos contagiadas cerca
+                */
                 if(personas.get(i).getState()==State.S && personas.get(j).getState()==State.I){
                     double e = Math.sqrt(Math.pow(personas.get(i).getX()-personas.get(j).getX(),2)+Math.pow(personas.get(i).getY()-personas.get(j).getY(),2));
+                    /**
+                    * si esta a distancia de contagio, se utiliza la probabilidad de contagio para ver si se infecta
+                    */
                     if(e < SimulatorConfig.D){
                         if(Math.random()<=SimulatorConfig.P0){
                             personas.get(i).infect();
@@ -52,27 +87,44 @@ public class Comuna {
                 }
             }
         }
+        /**
+        * calcula el estado de cada idividuo
+        */
         for(int i=0; i<personas.size();i++) {
             personas.get(i).computeNextState(delta_t);
         }
     }
+    /**
+    * actualiza el estado de cada idividuo
+    */
     public void updateState () {
         for(int i=0; i<personas.size();i++){
             personas.get(i).updateState();
         }
     }
+    /**
+    * actualiza la representación grafica de la comuna y todo en su interior
+    */
     public void updateView(){
         view.update();
     }
+    /**
+    * retorna el arreglo de individuos
+    */
     public ArrayList<Pedestrian> getPedestrian() {
         return personas;
     }
+
+    /**
+    * retorna el la representación grafica de la comuna
+    */
     public Group getView() {
         return view;
     }
-    public Pane getGraph(){
-        return graph;
-    }
+
+    /**
+    * retorna el estado de todos los individuos
+    */
     public String getIndState(){
         int sus=0,inf=0,rec=0;
         State status;
