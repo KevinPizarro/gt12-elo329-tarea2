8fc0b84dee4f7fac4590338328e768d3ea9899bc
Pablo_t <pablo.troncosop@sansano.usm.cl>
2021-06-10 22:31:36 -0400

    Se agregan los comentarios a los archivos correspondientes de la etapa 3

--- Stage3/Comuna.java
+++ Stage3/Comuna.java
@@ -1,148 +1,197 @@
-package Stage2;
+package Stage3;
 import java.util.ArrayList;
 
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.layout.Pane;
-
 /**
- * Genera la comuna donde se realizara el análisis
+ * Genera la comuna donde se realizará el análisis
  */
+
 public class Comuna {
     /**
-    * Arreglo de individuos
-    */
+     * Arreglo de individuos
+     */
     private ArrayList<Pedestrian> personas;
     /**
-    * Dimensiones del territorio de la comuna
-    */
-    private Rectangle2D territory; 
+     * Dimensiones del territorio de la comuna
+     */
+    private Rectangle2D territory;
     /**
      * Representación visual de la comuna
      */
     private ComunaView view;
+    /**
+     * Pane correspondiente al gráfico.
+     */
+    private Pane graph;
 
     public Comuna(){
         /**
-        * Constructor de la clase Comuna
-        */
+         * Constructor de la clase Comuna
+         */
         double width = SimulatorConfig.WIDTH;
         double length = SimulatorConfig.LENGTH;
         territory = new Rectangle2D(0,0, width, length);
         double speed = SimulatorConfig.SPEED;
         double deltaAngle = SimulatorConfig.DELTA_THETA;
-        view = new ComunaView(this); 
-                /**
-        * Crea el arreglo de individuos
-        */
+        view = new ComunaView(this);
+        /**
+         * Crea el arreglo de individuos
+         */
         personas = new ArrayList<Pedestrian>(SimulatorConfig.N);
         /**
-        * Infecta individuos según los datos del archivos
-        */
+         * Infecta individuos según los datos del archivo
+         */
         for(int i=0;i<SimulatorConfig.N;i++){
             if(i<SimulatorConfig.I){
                 Pedestrian p=new Pedestrian(this, speed, deltaAngle);
                 personas.add(p);
                 personas.get(i).infect();
+                if(i < SimulatorConfig.I*SimulatorConfig.M){
+                    personas.get(i).putmask();
+                }
             }
             else{
                 Pedestrian p=new Pedestrian(this, speed, deltaAngle);
                 personas.add(p);
+                if(i-SimulatorConfig.I < (SimulatorConfig.N-SimulatorConfig.I)*SimulatorConfig.M){
+                    personas.get(i).putmask();
+                }
             }
         }
-
-
+        graph = new Pane();  // to be completed in other stages.
     }
+
     /**
-    * retorna el ancho de la comuna
-    */
+     * retorna el ancho de la comuna
+     * @return
+     */
     public double getWidth() {
         return territory.getWidth();
     }
+
     /**
-    * retorna el alto de la comuna
-    */
+     * retorna el alto de la comuna
+     * @return
+     */
     public double getHeight() {
         return territory.getHeight();
     }
+
     /**
-    * revisa el estado de los individuos y los actualiza
-    */
+     * revisa el estado de los individuos y los actualiza, calculando cuando un individuo se contagia
+     * @param delta_t variación de tiempo con el que se calcula el siguiente estado
+     */
     public void computeNextState (double delta_t) {
         for(int i=0;i < personas.size();i++){
             for(int j=0; j< personas.size();j++){
-                /**
-                * revisa si hay individuos contagiadas cerca
-                */
                 if(personas.get(i).getState()==State.S && personas.get(j).getState()==State.I){
                     double e = Math.sqrt(Math.pow(personas.get(i).getX()-personas.get(j).getX(),2)+Math.pow(personas.get(i).getY()-personas.get(j).getY(),2));
-                    /**
-                    * si esta a distancia de contagio, se utiliza la probabilidad de contagio para ver si se infecta
-                    */
                     if(e < SimulatorConfig.D){
-                        if(Math.random()<=SimulatorConfig.P0){
-                            personas.get(i).infect();
+                        if((personas.get(i).getmask() == true) && (personas.get(j).getmask() == true)){
+                            if(Math.random() <= SimulatorConfig.P2){
+                                personas.get(i).infect();
+                            }
+                        }
+                        else if(((personas.get(i).getmask() == true) && (personas.get(j).getmask() == false)) || ((personas.get(i).getmask() == false) && (personas.get(j).getmask() == true))){
+                            if(Math.random() <= SimulatorConfig.P1){
+                                personas.get(i).infect();
+                            }
+                        } 
+                        else{
+                            if(Math.random() <= SimulatorConfig.P0){
+                                personas.get(i).infect();
+                            }
                         }
                     }
                 }
             }
         }
         /**
-        * calcula el estado de cada idividuo
-        */
+         * calcula el estado de cada individuo
+         */
         for(int i=0; i<personas.size();i++) {
             personas.get(i).computeNextState(delta_t);
         }
     }
     /**
-    * actualiza el estado de cada idividuo
-    */
+     * actualiza el estado de cada idividuo
+     */
     public void updateState () {
         for(int i=0; i<personas.size();i++){
             personas.get(i).updateState();
         }
     }
     /**
-    * actualiza la representación grafica de la comuna y todo en su interior
-    */
+     * actualiza la representación grafica de la comuna y todo en su interior
+     */
     public void updateView(){
         view.update();
     }
     /**
-    * retorna el arreglo de individuos
-    */
+     * retorna el arreglo de individuos
+     */
     public ArrayList<Pedestrian> getPedestrian() {
         return personas;
     }
-
     /**
-    * retorna el la representación grafica de la comuna
-    */
+     * retorna el estado de la representación grafica de la comuna
+     */
     public Group getView() {
         return view;
     }
 
     /**
-    * retorna el estado de todos los individuos
-    */
-    public String getIndState(){
-        int sus=0,inf=0,rec=0;
+     * retorna el pane del gráfico
+     */
+    public Pane getGraph(){
+        return graph;
+    }
+
+    /**
+     * retorna la cantidad de personas susceptibles
+     */
+    public int getSus(){
+        int sus=0;
         State status;
         for(int i=0; i<personas.size();i++){
             status=personas.get(i).getState();
-            switch (status) {
-                case R:
-                    rec++;
-                    break;
-                case I:
-                    inf++;
-                    break;
-                case S:
-                    sus++;
-                    break;
+            if(status == State.S){
+                sus++;
             }
         }
-        String s = inf + ",\t" + rec + ",\t" + sus;
-        return s;
+        System.out.println(sus);
+        return sus;
+    }
+
+    /**
+     * retorna la cantidad de infectados
+     */
+    public int getInf(){
+        int inf=0;
+        State status;
+        for(int i=0; i<personas.size();i++){
+            status=personas.get(i).getState();
+            if(status == State.I){
+                inf++;
+            }
+        }
+        return inf;
+    }
+
+    /**
+     * retorna la cantidad de recuperados
+     */
+    public int getRec(){
+        int rec=0;
+        State status;
+        for(int i=0; i<personas.size();i++){
+            status=personas.get(i).getState();
+            if(status == State.R){
+                rec++;
+            }
+        }
+        return rec;
     }
  }
